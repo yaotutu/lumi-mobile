969b880017f8eddb3448276388a41b1a2b8d0f8f
yaotutu <957810668@qq.com>
2025-11-15 01:07:47 +0800

    Set discover tab as initial route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  initialRouteName: '(tabs)',
 };
 
 export default function RootLayout() {
